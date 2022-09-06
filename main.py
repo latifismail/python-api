9e254728b9f3d65498e2705f9fc66c3e972dcca2
Latif Ismail <latifismail.id@gmail.com>
2022-09-06 23:10:38 +0700

    Create basic GET and POST request

--- main.py
+++ main.py
@@ -1,8 +1,50 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+import uuid
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+posts = []
+
+def find_post(id):
+    result = next((post for post in posts if post["id"] == id), None)
+    return result
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": posts}
+
+@app.get("/posts/latest")
+def get_latest_post():
+    post = posts[-1]
+    return {"data": post}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    post = find_post(id)
+    if post == None:
+        return {"message": "Post not found"}
+
+    return {"data": post}
+
+@app.post("/posts")
+def create_posts(payload: Post):
+    post = payload.dict()
+    post['id'] = str(uuid.uuid4())
+    posts.append(post)
+
+    return {
+        "message": "success",
+        "data": post
+    }
